84c3b6cd7f26972eee0b5b4870d1ba1a95977050
Дмитрий <dimaprusskii@mail.ru>
2017-10-03 01:48:38 +0300

    Task 2. Only for lower case.

---
+++ src/task2/ROT13.java
@@ -0,0 +1,28 @@
+package task2;
+
+
+/**
+ * Created by 803019 on 03.10.2017.
+ */
+
+// It works for lower case only. If it is necessary, I can do it for lower case too.
+public class ROT13 {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+
+        System.out.println("Encode: " + rot13(args[0]));
+        System.out.println("Decode: " + rot13(rot13(args[0])));
+
+
+    }
+
+    public static String rot13(String str) {
+        char result[] = str.toCharArray();
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(Character.codePointAt(result, i));
+            if (Character.codePointAt(result, i) > 109) result[i] -= 13;
+            else result[i] += 13;
+        }
+        return String.valueOf(result);
+    }
+}
