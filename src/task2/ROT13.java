e836a0e2a200186ab562d29e36f06627d65c378c
Дмитрий <dimaprusskii@mail.ru>
2017-11-02 21:31:54 +0300

    add handling capital letters

--- src/task2/ROT13.java
+++ src/task2/ROT13.java
@@ -3,20 +3,26 @@ package task2;
 // It works for lower case only. If it is necessary, I can do it for lower case too.
 public class ROT13 {
     public static void main(String[] args) {
-        System.out.println(args[0]);
 
-        System.out.println("Encode: " + rot13(args[0]));
-        System.out.println("Decode: " + rot13(rot13(args[0])));
+        String msg = "ASfewqarARRF";
+        System.out.println(msg);
 
+        System.out.println("Encode: " + rot13(msg));
+        System.out.println("Decode: " + rot13(rot13(msg)));
 
     }
 
     public static String rot13(String str) {
         char result[] = str.toCharArray();
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(Character.codePointAt(result, i));
-            if (Character.codePointAt(result, i) > 109) result[i] -= 13;
-            else result[i] += 13;
+        try {
+            for (int i = 0; i < result.length; i++) {
+                int a = Character.codePointAt(result, i);
+                if (a > 77 && a < 90 || a > 109 && a < 123) result[i] -= 13;
+                else if (a > 64 && a < 78 || a > 96 && a < 110) result[i] += 13;
+                else throw new Exception("wrong letters");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return String.valueOf(result);
     }
