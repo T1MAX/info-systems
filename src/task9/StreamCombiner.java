9b661cdd321c995e1d6cf0deb5914f8f8cecc77b
Дмитрий <dimaprusskii@mail.ru>
2017-11-12 20:56:52 +0300

    task9 resolved

--- src/task9/StreamCombiner.java
+++ src/task9/StreamCombiner.java
@@ -1,24 +1,50 @@
 package task9;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class StreamCombiner {
     public static void main(String[] args) {
         Stream first = Stream.builder().add(1).add(3).add(9).build();
         Stream second = Stream.builder().add(2).add(4).add(6).add(8).build();
 
-        zip(first, second).forEach(System.out::println);
+        zip(first, second, (a, b) -> a + ", " + b).forEach(System.out::println);
     }
 
-    public static Stream zip(Stream first, Stream second) {
-        Object[] firstArray = first.toArray();
-        Object[] secondArray = second.toArray();
-        Object[] resultArray = new Object[Math.min(firstArray.length, secondArray.length) * 2];
-        for (int i = 0, j = 0; i < resultArray.length - 1 && j < Math.min(firstArray.length, secondArray.length); j++) {
-            resultArray[i++] = firstArray[j];
-            resultArray[i++] = secondArray[j];
-        }
-        return Arrays.stream(resultArray);
+    public static<A, B, C> Stream<C> zip(Stream<? extends A> a,
+                                         Stream<? extends B> b,
+                                         BiFunction<? super A, ? super B, ? extends C> zipper) {
+        Objects.requireNonNull(zipper);
+        Spliterator<? extends A> aSpliterator = Objects.requireNonNull(a).spliterator();
+        Spliterator<? extends B> bSpliterator = Objects.requireNonNull(b).spliterator();
+
+        // Zipping looses DISTINCT and SORTED characteristics
+        int characteristics = aSpliterator.characteristics() & bSpliterator.characteristics() &
+                ~(Spliterator.DISTINCT | Spliterator.SORTED);
+
+        long zipSize = ((characteristics & Spliterator.SIZED) != 0)
+                ? Math.min(aSpliterator.getExactSizeIfKnown(), bSpliterator.getExactSizeIfKnown())
+                : -1;
+
+        Iterator<A> aIterator = Spliterators.iterator(aSpliterator);
+        Iterator<B> bIterator = Spliterators.iterator(bSpliterator);
+        Iterator<C> cIterator = new Iterator<C>() {
+            @Override
+            public boolean hasNext() {
+                return aIterator.hasNext() && bIterator.hasNext();
+            }
+
+            @Override
+            public C next() {
+                return zipper.apply(aIterator.next(), bIterator.next());
+            }
+        };
+
+        Spliterator<C> split = Spliterators.spliterator(cIterator, zipSize, characteristics);
+        return (a.isParallel() || b.isParallel())
+                ? StreamSupport.stream(split, true)
+                : StreamSupport.stream(split, false);
     }
 }
